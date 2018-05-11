0237f38df2fc0cfbd361906acc86b1cb030e81c9
howard <howarder3@gmail.com>
2018-05-11 16:02:27 +0800

    test2

--- update.py
+++ update.py
@@ -1,12 +1,24 @@
 # update.py
 import time
+import gspread
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+def auth_gss_client(path, scopes):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(path,scopes)
+    return gspread.authorize(credentials)
+
+auth_json_path = "./auth.json"
+gss_scopes = ['https://spreadsheets.google.com/feeds']
+
+gss_client = auth_gss_client(auth_json_path, gss_scopes)
 
 def update_sheet(gss_client, key, today, item, price):
     wks = gss_client.open_by_key(key)
     sheet = wks.sheet1
     sheet.insert_row([today, item, price], 2)
 
-spreadsheet_key = 19nQvlQIGRIoGELFxGfHWazG45DM7D2GccZg8wlD85_g	
+spreadsheet_key = "19nQvlQIGRIoGELFxGfHWazG45DM7D2GccZg8wlD85_g"	
 # spreadsheet_key_path = 'spreadsheet_key'
 
 
